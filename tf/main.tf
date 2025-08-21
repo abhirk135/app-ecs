084239b6c7f0f4fea39e3d147a3bcb1166233337
abhirk135 <abhirk135@gmail.com>
2025-08-20 22:15:05 -0400

    Deploy App

---
+++ tf/main.tf
@@ -0,0 +1,95 @@
+resource "aws_ecr_repository" "ark_repo" {
+  name = var.ecr_repo_name
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+resource "aws_ecr_lifecycle_policy" "ark_lifecycle_policy" {
+  repository = aws_ecr_repository.ark_repo.name
+  policy     = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images older than 30 days"
+        selection    = {
+          tagStatus = "untagged"
+          countType = "sinceImagePushed"
+          countUnit = "days"
+          countNumber = 30
+        }
+        action      = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+#### Permissions setup for ECS ####################################
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ecs_task_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+##### Task definition and Service #####################
+
+resource "aws_ecs_task_definition" "ark_task" {
+  family                   = "ark"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "ark"
+      image     = "${aws_ecr_repository.ark_repo.repository_url}:latest"
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_cluster" "ark_cluster" {
+  name = var.cluster_name
+}
+
+resource "aws_ecs_service" "ark_service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.ark_cluster.id
+  task_definition = aws_ecs_task_definition.ark_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = var.security_group_ids
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_policy]
+}
---
+++ tf/variable.tf
@@ -0,0 +1,47 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ecr_repo_name" {
+  description = "ECR repository name"
+  type        = string
+  default     = "ark-hw-repo"
+}
+
+variable "ecr_image_tag" {
+  description = "ECR image tag"
+  type        = string
+  default     = "latest"
+}
+
+variable "cluster_name" {
+  default = "ark-hw-cluster"
+}
+variable "service_name" {
+  default = "ark-hw-service"
+}
+variable "container_name" {
+  default = "ark-hw-container"
+}
+variable "container_port" {
+  default = 8081
+}
+variable "subnet_ids" {
+  description = "List of subnet IDs for ECS tasks"
+  type        = list(string)
+  default     = [
+    "subnet-0c5f890519d4f7f7f",
+    "subnet-029f5515fc8771089",
+    "subnet-06abea096147f9567"
+  ]
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs for ECS tasks"
+  type        = list(string)
+  default     = [
+    "sg-0fca373cc94c770ee"
+  ]
+}
